ad5db8ab33f586ec070617c614d64b389e8c7f93
Colin <colin.morritts@gmail.com>
2025-03-19 13:26:43 -0500

    added useeffect to app.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, TextInput, Button, StyleSheet, ScrollView, TouchableOpacity, FlatList } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
