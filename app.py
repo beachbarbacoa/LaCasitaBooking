2d634be15c83eff9722b94154c4320517482817d
Colin <colin.morritts@gmail.com>
2025-03-30 18:18:22 -0500

    updated app.py to resolve errors with connecting to the server

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 class Reservation(db.Model):
+    __tablename__ = 'reservation'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
